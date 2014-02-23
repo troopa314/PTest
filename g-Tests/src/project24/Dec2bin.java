573ce2e10a15760dbfe4cea6e1d6f72a389b3375
Troopa <Troopa@Troopa-PC>
2014-02-23 11:41:52 +0100

    Project #24 now working with all bases from 1-10

--- g-Tests/src/project24/Dec2bin.java
+++ g-Tests/src/project24/Dec2bin.java
@@ -9,21 +9,31 @@ import java.util.Scanner;
 public class Dec2bin {
 	private static String s = "";
 
-	private static long getRemainder(long n) {
-		return n % 2;
+	private static long getRemainder(long n, int givenDivisor) {
+		return n % givenDivisor;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Number you want to convert:");
-		long n = sc.nextLong();
-		System.out.println("In binary, your number is:" + convertToBinary(n));
+		long givenNumber = sc.nextLong();
+		System.out.print("Base of targeted System:");
+		int givenBase = sc.nextInt();
+		System.out.println("In base "+ givenBase +", your number is:" + convertToGivenBase(givenNumber, givenBase));
 	}
 	
 	public static String convertToBinary(long n) {
-		s = s + Long.toString(getRemainder(n));
+		s = s + Long.toString(getRemainder(n, 2));
 		if(n != 0) {
 			convertToBinary((int)Math.floor(n / 2));
+		}
+		return new StringBuilder(s).reverse().toString();
+	}
+	
+	public static String convertToGivenBase(long n, int base) {
+		s = s + Long.toString(getRemainder(n, base));
+		if(n != 0) {
+			convertToGivenBase((int)Math.floor(n / base), base);
 		}
 		return new StringBuilder(s).reverse().toString();
 	}
